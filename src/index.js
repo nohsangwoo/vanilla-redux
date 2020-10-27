f5c89c4a7e8f17e47c16e64965f312d2a517af9c
nohsangwoo <nsgr12@naver.com>
2020-10-27 17:08:31 +0900

    improvement

--- src/index.js
+++ src/index.js
@@ -6,29 +6,42 @@ const reset = document.getElementById("reset");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+const RESET = "RESET";
+
 // modify global data
+// 전송받은 data는 action으로 저장됨
+// 첫번째 인자는 current state이다.
 const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else if (action.type === "RESET") {
-    return (count = 0);
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    case RESET:
+      return (count = 0);
+    default:
+      return count;
   }
 };
 
+// data를 control하기위한 세팅
 const countStore = createStore(countModifier);
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
-reset.addEventListener("click", () => countStore.dispatch({ type: "RESET" }));
+// 이벤트리스터 호출
+//dispatch를 이용하여 countModifier로 데이터를 전송가능
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+reset.addEventListener("click", () => countStore.dispatch({ type: RESET }));
+
 const onChange = () => {
+  // countModifier의 값을 가져옴( data)
   number.innerText = countStore.getState();
 };
+
+// 여기서 countModifider를 구독
 countStore.subscribe(onChange);
 
-// countModifier의 값을 가져옴( data)
 // console.log(countStore.getState());
